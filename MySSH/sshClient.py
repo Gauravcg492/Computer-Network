10ecd7ffc8a116912799269b512c4815715a69be
gauravcg492 <officialgauravcg492@gmail.com>
2019-11-19 23:55:31 +0530

    test 2

--- MySSH/sshClient.py
+++ MySSH/sshClient.py
@@ -3,15 +3,22 @@ from socket import *
 
 if __name__ == "__main__":
     #Establishing connection
-    serverPort = int(input("Enter port number: "))
-    serverName = input("Enter Server address: ")
-
+    #serverPort = int(input("Enter port number: "))
+    #serverName = input("Enter Server address: ")
+    serverPort = 19999
+    serverName = '127.0.0.1'
     clientSocket = socket(AF_INET,SOCK_STREAM)
     clientSocket.connect((serverName,serverPort))
-    msg = input()
-    clientSocket.send(msg.encode())
-    msg = clientSocket.recv(4096)
-    print(msg.decode())
+    while True:
+        msg = input('> ')
+        clientSocket.send(msg.encode())
+        print('receiving...')
+        output = clientSocket.recv(4096).decode()
+        if('cd' not in msg and output == ''):
+            break
+        if(len(output) > 0):
+            print(output)
+            output = ''
     #clientSocket.close()
 
 
--- MySSH/sshServer.py
+++ MySSH/sshServer.py
@@ -1,5 +1,5 @@
 from socket import *
-import subprocess
+import os
 
 if __name__ == "__main__":
     #setting server
@@ -8,8 +8,24 @@ if __name__ == "__main__":
     s.listen(5)
     print("listening: ")
     while True:
-        clientSocket, address = s.accept()
-        inp = clientSocket.recv(4096)
-        output = subprocess.check_output(inp.decode(),shell=True)
-        clientSocket.send(output)
+        clientSocket, address = s.accept()        
+        while True:
+            print('Receiving..')
+            inp = clientSocket.recv(4096).decode()
+            output = 'NULL'
+            if(inp.lower() == 'exit'):
+                break
+            if(len(inp) >= 2 and 'cd' == inp[:2]):
+                if inp == 'cd':
+                    print('calling cd here')
+                    os.chdir(os.path.expanduser('~'))
+                else:
+                    print('changing directory')
+                    os.chdir(inp[3:])
+                output = 'cd'
+            elif len(inp) > 0:
+                print('calling systems')
+                output = os.popen(inp).read()
+            print('sending ',output)
+            clientSocket.send(output.encode())
         clientSocket.close()
