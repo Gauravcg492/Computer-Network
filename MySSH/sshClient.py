6adaabad7bcaa8e6280173ed0af0752e2576cff1
gauravcg492 <officialgauravcg492@gmail.com>
2019-11-21 10:31:17 +0530

    test4

--- MySSH/sshClient.py
+++ MySSH/sshClient.py
@@ -3,6 +3,29 @@ from Cryptodome.PublicKey import RSA
 from Cryptodome.Cipher import AES,PKCS1_OAEP
 #from Cryptodome.Random import get_random_bytes
 
+def authenticate(clientSocket,user):
+	clientSocket.send(user.encode())
+	out = int(clientSocket.recv(4096).decode())
+	if out:
+		print('Authenticating')
+		enc_sk = clientSocket.recv(4096).decode()
+		return enc_sk
+	else:
+		print('Invalid UserName')
+		return 0
+
+def sendMessage(clientSocket,msg,sk):
+	encryptor = AES.new(sk,AES.MODE_EAX)
+	enc_msg,tag = encryptor.encrypt_and_digest(msg.encode())
+	[clientSocket.send(x) for x in (encryptor.nonce,tag,enc_msg)]
+
+def receiveMessage(clientSocket,sk):
+	nonce = clientSocket.recv(4096)
+	tag = clientSocket.recv(4096)
+	enc_msg = clientSocket.recv(4096)
+	decryptor = AES.new(sk,AES.MODE_EAX,nonce)
+	msg = decryptor.decrypt_and_verify(enc_msg,tag)
+	return msg.decode()
 
 if __name__ == "__main__":
     #Establishing connection
@@ -13,17 +36,19 @@ if __name__ == "__main__":
     user = input('User Name: ')
     clientSocket = s.socket(s.AF_INET,s.SOCK_STREAM)
     clientSocket.connect((serverName,serverPort))
-
-    while True:
-        msg = input('> ')
-        clientSocket.send(msg.encode())
-        #print('receiving...')
-        output = clientSocket.recv(4096).decode()
-        if('cd' not in msg and output == ''):
-            break
-        if(len(output) > 0):
-            print(output)
-            output = ''
+	sk = authenticate(clientSocket,user)
+	if not sk == 0:
+		while True:
+		    msg = input('> ')
+		    sendMessage(clientSocket,msg,sk)
+		    #clientSocket.send(msg.encode())
+		    #print('receiving...')
+		    output = clientSocket.recv(4096).decode()
+		    if('cd' not in msg and output == ''):
+		        break
+		    if(len(output) > 0):
+		        print(output)
+		        output = ''
     #clientSocket.close()
 
 
--- MySSH/sshServer.py
+++ MySSH/sshServer.py
@@ -7,11 +7,17 @@ from Cryptodome.Cipher import AES,PKCS1_OAEP
 from Cryptodome.Random import get_random_bytes
 
 
-def authenticate(clientSocket,sessionKey):
-    publicKey = clientSocket.recv(4096).decode()
-    encrytor = PKCS1_OAEP.new(publicKey)
-    encrypted_SK = encrytor.encrypt(sessionKey)
-    clientSocket.send(encrypted_SK)
+def authenticate(clientSocket,sessionKey,username):
+	user = clientSocket.recv(4096).decode()
+	if(username == user):
+		clientSocket.send('Authenticating'.encode())
+		publicKey = clientSocket.recv(4096).decode()
+		encrytor = PKCS1_OAEP.new(publicKey)
+		encrypted_SK = encrytor.encrypt(sessionKey)
+		clientSocket.send(encrypted_SK)
+	else:
+		clientSocket.send('Invalid user'.decode())
+		return 0
 
 
 if __name__ == "__main__":
@@ -25,28 +31,30 @@ if __name__ == "__main__":
     while True:
         clientSocket, address = server.accept()
         sessionKey = get_random_bytes(16)
-        authenticate(clientSocket,sessionKey)        
-        while True:
-            #print('Receiving..')
-            cmd = clientSocket.recv(4096).decode()
-            output = b'NULL'
-            if(cmd.lower() == 'exit'):
-                break
-            if(len(cmd) >= 2 and 'cd' == cmd[:2]):
-                if cmd == 'cd':
-                    #print('calling cd here')
-                    os.chdir(os.path.expanduser('/')+'/home/'+userName)
-                else:
-                    print('changing directory')
-                    try:
-                        os.chdir(cmd[3:])
-                    except:
-                        output = 'FileNotFoundError: [Errno 2] No such file or directory: ' + cmd[3:]
-            elif len(cmd) > 0:
-                print('calling systems')
-                #cwd = os.getcwd()
-                #print(cwd)
-                output = subprocess.check_output(cmd,shell=True)
-            print('sending ',output)
-            clientSocket.send(output)
+        if authenticate(clientSocket,sessionKey,username):        
+		    while True:
+		        #print('Receiving..')
+		        cmd = clientSocket.recv(4096).decode()
+		        output = b'NULL'
+		        if(cmd.lower() == 'exit'):
+		            break
+		        if(len(cmd) >= 2 and 'cd' == cmd[:2]):
+		            if cmd == 'cd':
+		                #print('calling cd here')
+		                os.chdir(os.path.expanduser('/')+'/home/'+userName)
+		            else:
+		                print('changing directory')
+		                try:
+		                    os.chdir(cmd[3:])
+		                except:
+		                    output = 'FileNotFoundError: [Errno 2] No such file or directory: ' + cmd[3:]
+		        elif len(cmd) > 0:
+		            print('calling systems')
+		            #cwd = os.getcwd()
+		            #print(cwd)
+		            output = subprocess.check_output(cmd,shell=True)
+		        print('sending ',output)
+		        clientSocket.send(output)
+		else:
+			clientSocket.send('Unauthorized User'.encode())
         clientSocket.close()
