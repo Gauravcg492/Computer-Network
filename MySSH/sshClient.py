07c64c3ca8d525028a82dcae6930ec81eab03591
gauravcg492 <officialgauravcg492@gmail.com>
2019-11-21 19:25:13 +0530

    Working

--- MySSH/sshClient.py
+++ MySSH/sshClient.py
@@ -1,54 +1,68 @@
 import socket as s
 from Cryptodome.PublicKey import RSA
-from Cryptodome.Cipher import AES,PKCS1_OAEP
+from Cryptodome.Cipher import AES, PKCS1_OAEP
 #from Cryptodome.Random import get_random_bytes
 
-def authenticate(clientSocket,user):
+
+def authenticate(clientSocket, user):
 	clientSocket.send(user.encode())
 	out = int(clientSocket.recv(4096).decode())
 	if out:
 		print('Authenticating')
-		enc_sk = clientSocket.recv(4096).decode()
-		return enc_sk
+		key = RSA.generate(2048)
+		clientSocket.send(key.publickey().export_key())
+		enc_sk = clientSocket.recv(4096)
+		#print(enc_sk)
+		decryptor = PKCS1_OAEP.new(key)
+		sk = decryptor.decrypt(enc_sk)		
+		return sk
 	else:
 		print('Invalid UserName')
 		return 0
 
-def sendMessage(clientSocket,msg,sk):
-	encryptor = AES.new(sk,AES.MODE_EAX)
-	enc_msg,tag = encryptor.encrypt_and_digest(msg.encode())
-	[clientSocket.send(x) for x in (encryptor.nonce,tag,enc_msg)]
 
-def receiveMessage(clientSocket,sk):
-	nonce = clientSocket.recv(4096)
-	tag = clientSocket.recv(4096)
-	enc_msg = clientSocket.recv(4096)
-	decryptor = AES.new(sk,AES.MODE_EAX,nonce)
-	msg = decryptor.decrypt_and_verify(enc_msg,tag)
+def sendMessage(clientSocket, msg, sk):
+	encryptor = AES.new(sk, AES.MODE_EAX)
+	enc_msg, tag = encryptor.encrypt_and_digest(msg.encode())
+	#print('sending\n',encryptor.nonce + b'|$' + tag + b'|$' + enc_msg)
+	clientSocket.sendall(encryptor.nonce + b'|$' + tag + b'|$' + enc_msg)
+
+
+def receiveMessage(clientSocket, sk):
+	nonce,tag,enc_msg =[i for i in clientSocket.recv(4096).strip(b'|$').split(b'|$')]
+	#print(nonce)
+	decryptor = AES.new(sk, AES.MODE_EAX, nonce)
+	msg = decryptor.decrypt(enc_msg)
 	return msg.decode()
 
+
 if __name__ == "__main__":
-    #Establishing connection
+    # Establishing connection
     #serverPort = int(input("Enter port number: "))
     #serverName = input("Enter Server address: ")
-    serverPort = int(input('Enter port number: '))
-    serverName = '127.0.0.1'
-    user = input('User Name: ')
-    clientSocket = s.socket(s.AF_INET,s.SOCK_STREAM)
-    clientSocket.connect((serverName,serverPort))
-	sk = authenticate(clientSocket,user)
+	serverPort = int(input('Enter port number: '))
+	serverName = '127.0.0.1'
+	user = input('User Name: ')
+	clientSocket = s.socket(s.AF_INET, s.SOCK_STREAM)
+	clientSocket.connect((serverName, serverPort))
+	sk = authenticate(clientSocket, user)
 	if not sk == 0:
 		while True:
-		    msg = input('> ')
-		    sendMessage(clientSocket,msg,sk)
-		    #clientSocket.send(msg.encode())
-		    #print('receiving...')
-		    output = clientSocket.recv(4096).decode()
-		    if('cd' not in msg and output == ''):
-		        break
-		    if(len(output) > 0):
-		        print(output)
-		        output = ''
-    #clientSocket.close()
-
-
+			msg = input('> ')
+			#print('Sending ',msg)
+			sendMessage(clientSocket, msg, sk)
+			#clientSocket.send(msg.encode())
+			#print('receiving...')
+			if msg.lower() == 'exit':
+				excess = clientSocket.recv(4096)
+				break
+			output = receiveMessage(clientSocket, sk)
+			#output = clientSocket.recv(4096).decode()
+			if('cd' not in msg and output == ''):
+				break
+			if(len(output) > 0 and output != 'NULL' ):
+				print(output)
+				output = ''
+			else:
+				print()
+    # clientSocket.close()
--- MySSH/sshServer.py
+++ MySSH/sshServer.py
@@ -3,58 +3,77 @@ import os
 import subprocess
 import getpass
 from Cryptodome.PublicKey import RSA
-from Cryptodome.Cipher import AES,PKCS1_OAEP
+from Cryptodome.Cipher import AES, PKCS1_OAEP
 from Cryptodome.Random import get_random_bytes
 
 
-def authenticate(clientSocket,sessionKey,username):
+def authenticate(clientSocket, sessionKey, username):
 	user = clientSocket.recv(4096).decode()
+	#print(user,username)
 	if(username == user):
-		clientSocket.send('Authenticating'.encode())
-		publicKey = clientSocket.recv(4096).decode()
+		clientSocket.send('1'.encode())
+		publicKey = clientSocket.recv(4096)
+		publicKey = RSA.import_key(publicKey)
 		encrytor = PKCS1_OAEP.new(publicKey)
 		encrypted_SK = encrytor.encrypt(sessionKey)
+		#print(encrypted_SK)
 		clientSocket.send(encrypted_SK)
+		return 1
 	else:
-		clientSocket.send('Invalid user'.decode())
+		clientSocket.send('0'.encode())
 		return 0
 
 
+def sendMessage(clientSocket, msg, sk):
+	encryptor = AES.new(sk, AES.MODE_EAX)
+	enc_msg, tag = encryptor.encrypt_and_digest(msg)
+	#print('sending\n',encryptor.nonce + b'|$' + tag + b'|$' + enc_msg)
+	clientSocket.sendall(encryptor.nonce + b'|$' + tag + b'|$' + enc_msg)
+
+
+def receiveMessage(clientSocket, sk):
+    nonce,tag,enc_msg =[i for i in clientSocket.recv(4096).strip(b'|$').split(b'|$')]
+    decryptor = AES.new(sk, AES.MODE_EAX, nonce)
+    msg = decryptor.decrypt(enc_msg)
+    return msg.decode()
+
+
 if __name__ == "__main__":
-    #setting server
-    serverPort = int(input('Enter the port number: '))
-    server = s.socket(s.AF_INET,s.SOCK_STREAM)
-    server.bind(('',serverPort))
-    server.listen(5)
-    userName = getpass.getuser()
-    #print("listening: ")
-    while True:
-        clientSocket, address = server.accept()
-        sessionKey = get_random_bytes(16)
-        if authenticate(clientSocket,sessionKey,username):        
-		    while True:
-		        #print('Receiving..')
-		        cmd = clientSocket.recv(4096).decode()
-		        output = b'NULL'
-		        if(cmd.lower() == 'exit'):
-		            break
-		        if(len(cmd) >= 2 and 'cd' == cmd[:2]):
-		            if cmd == 'cd':
-		                #print('calling cd here')
-		                os.chdir(os.path.expanduser('/')+'/home/'+userName)
-		            else:
-		                print('changing directory')
-		                try:
-		                    os.chdir(cmd[3:])
-		                except:
-		                    output = 'FileNotFoundError: [Errno 2] No such file or directory: ' + cmd[3:]
-		        elif len(cmd) > 0:
-		            print('calling systems')
-		            #cwd = os.getcwd()
-		            #print(cwd)
-		            output = subprocess.check_output(cmd,shell=True)
-		        print('sending ',output)
-		        clientSocket.send(output)
+	# setting server
+	serverPort = int(input('Enter the port number: '))
+	server = s.socket(s.AF_INET, s.SOCK_STREAM)
+	server.bind(('', serverPort))
+	server.listen(5)
+	userName = getpass.getuser()
+	while True:
+		clientSocket, address = server.accept()
+		sessionKey = get_random_bytes(16)
+		if authenticate(clientSocket, sessionKey, userName):
+			while True:
+				#print('Receiving..')
+				cmd = receiveMessage(clientSocket, sessionKey)
+				output = b'NULL'
+				if(cmd.lower() == 'exit'):
+					break
+				if(len(cmd) >= 2 and 'cd' == cmd[:2]):
+					if cmd == 'cd':
+						#print('calling cd here')
+						os.chdir(os.path.expanduser('/'))
+					else:
+						#print('changing directory')
+						try:
+							os.chdir(cmd[3:])
+						except:
+							output = ('FileNotFoundError: [Errno 2] No such file or directory: ' + cmd[3:])
+				elif len(cmd) > 0:
+					#print('calling systems')
+					#print('Executing cmd ',cmd)
+					output = subprocess.check_output(cmd, shell=True)
+				if output == '':
+					output = b'NULL'
+				#print('sending ', output)
+				#clientSocket.send(output)
+				sendMessage(clientSocket,output,sessionKey)
 		else:
 			clientSocket.send('Unauthorized User'.encode())
-        clientSocket.close()
+		clientSocket.close()
